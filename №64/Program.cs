fc25daf0952089be87a185fa6d36eca0a91fa1d3
ilya <ilya-ryabov03@yandex.ru>
2022-11-17 00:12:21 +0300

    homework

---
+++ №64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ??"");
+
+string PrintNatur(int start, int end)
+{
+    if(start==end)
+    {
+        return start.ToString();
+    }
+    return (start + ", " + PrintNatur(start + 1, end));
+}
+Console.WriteLine(PrintNatur(1, N));
+
---
+++ №66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ??"");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ??"");
+int result = 0;
+int SumNatur(int start, int end)
+{
+    for (int i = start; i<=end; i++)
+    {
+        result += i;
+    } 
+    return result;
+}
+Console.WriteLine(SumNatur(M,N));
